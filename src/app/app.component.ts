8a6513578af5186d6605062f9a4f22f0e8ae0e5a
kbalu99 <kbalu99@gmail.com>
2018-09-17 01:35:22 -0400

    Signed-off-by: kbalu99 <kbalu99@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent {  
   title = 'uiuxdemos';
+  mockups = [
+    {
+        "name": "Search Programs",
+        "link": "https://kbalu99.github.io/uiux/Search.html",
+        "preview":  "./assets/images/Search_Pgms.png"
+    },
+
+    {
+        "name": "Duplicate",
+        "link": "https://kbalu99.github.io/uiux/Search.html",
+        "preview":  "./assets/images/Search_Pgms.png"
+    }
+];
 }
